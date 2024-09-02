fac9771aaf1574287397594fc97ba77de84da5af
Mark Skelton <markskelton10@gmail.com>
2024-09-02 10:14:44 +0100

    Create to_s method for board
    Using . as place holders in initialize. Using constants so that all the
    information is contained in one place for easier editing.

--- lib/board.rb
+++ lib/board.rb
@@ -2,11 +2,21 @@
 
 # Board conatins information about the guesses made and hints given
 # Constants: Guess colors available, hint colours available, number of guesses possible,
-#            and the number of guess and hint pegs possible
+#            and the number of guess and hint pegs possible per row
 # Methods include to_s and accessors for the guesses and hints
 class Board
+  MAX_GUESSES = 12
+  GUESSES_PER_ROW = 4
+  HINTS_PER_ROW = 4
+
   def initialize
-    @guesses = Array.new(12) { Array.new(4) }
-    @hints = Array.new(12) { Array.new(4) }
+    @guesses = Array.new(MAX_GUESSES) { Array.new(GUESSES_PER_ROW, '.') }
+    @hints = Array.new(MAX_GUESSES) { Array.new(HINTS_PER_ROW, '.') }
+  end
+
+  def to_s
+    (0...MAX_GUESSES).reduce('GUESSES | HINTS') do |board, idx|
+      "#{board}\n#{@guesses[idx].join(' ')} | #{@hints[idx].join(' ')}"
+    end
   end
 end
