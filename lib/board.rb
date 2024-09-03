9cf7a9cfb2226072db84a0aeb29111f00ffc2160
Mark Skelton <markskelton10@gmail.com>
2024-09-03 12:32:48 +0100

    Combine two constants into one in Board
    Combined GUESSES_PER_ROW and HINTS_PER_ROW into PIN_GROUP_SIZE, since
    one is always equal to the other. This means any changes only need to be
    made in one place.

--- lib/board.rb
+++ lib/board.rb
@@ -9,17 +9,16 @@ class Board
   include PinCreatable
 
   MAX_GUESSES = 12
-  GUESSES_PER_ROW = 4
-  HINTS_PER_ROW = 4
-  COMBINATIONS = GUESS_COLORS.keys.repeated_permutation(GUESSES_PER_ROW).map { |perm| perm }.freeze
+  PIN_GROUP_SIZE = 4
+  COMBINATIONS = GUESS_COLORS.keys.repeated_permutation(PIN_GROUP_SIZE).map { |perm| perm }.freeze
 
   attr_reader :latest_guess, :latest_hint
 
   def initialize
     @latest_guess = nil
     @latest_hint = nil
-    @guesses = Array.new(MAX_GUESSES) { Array.new(GUESSES_PER_ROW, '.') }
-    @hints = Array.new(MAX_GUESSES) { Array.new(HINTS_PER_ROW, '.') }
+    @guesses = Array.new(MAX_GUESSES) { Array.new(PIN_GROUP_SIZE, '.') }
+    @hints = Array.new(MAX_GUESSES) { Array.new(PIN_GROUP_SIZE, '.') }
   end
 
   def add_guess(guess, guess_number)
@@ -29,7 +28,7 @@ class Board
 
   def add_hint(red_pins, white_pins, guess_number)
     @latest_hint = Array.new(red_pins, 'r').concat(Array.new(white_pins, 'w'))
-    blanks = HINTS_PER_ROW - red_pins - white_pins
+    blanks = PIN_GROUP_SIZE - red_pins - white_pins
     @hints[guess_number] = create_hint_pins(@latest_hint).concat(Array.new(blanks, '.'))
   end
 
